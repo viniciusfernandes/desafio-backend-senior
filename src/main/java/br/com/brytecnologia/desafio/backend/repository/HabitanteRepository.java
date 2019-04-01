25d51332fb1c2cd804cd51c65ea8081b9970d1b9
vinicius <viniciussf@hotmail.com>
2019-04-01 05:21:11 -0300

    Correcao de bug na pesquisa de habitante se endereco

--- src/main/java/br/com/brytecnologia/desafio/backend/repository/HabitanteRepository.java
+++ src/main/java/br/com/brytecnologia/desafio/backend/repository/HabitanteRepository.java
@@ -20,7 +20,7 @@ public class HabitanteRepository {
 
 	public Habitante findByCodigo(String codigo) {
 		try {
-			return em.createQuery("select h from Habitante h join fetch h.enderecos where h.codigo=:codigo", Habitante.class)
+			return em.createQuery("select h from Habitante h left join fetch h.enderecos where h.codigo=:codigo", Habitante.class)
 					.setParameter("codigo", codigo).getSingleResult();
 		} catch (NoResultException | NonUniqueResultException e) {
 			return null;
@@ -28,7 +28,7 @@ public class HabitanteRepository {
 	}
 
 	public List<Habitante> findAll() {
-		return em.createQuery("select h from Habitante h join fetch h.enderecos", Habitante.class).getResultList();
+		return em.createQuery("select h from Habitante h left join fetch h.enderecos", Habitante.class).getResultList();
 	}
 
 	public Habitante save(Habitante habitante) {
